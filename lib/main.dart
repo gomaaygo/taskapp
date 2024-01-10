12d0c17708d47c78160f4b44793cab43ba7e5739
Maiury Garcia <maiurygarcia@gmail.com>
2024-01-10 16:58:25 -0300

    feat: Added progress

--- lib/main.dart
+++ lib/main.dart
@@ -22,22 +22,39 @@ class MyApp extends StatelessWidget {
           title: const Text('Tarefas'),
         ),
         body: ListView(
-          children: [Task('Aprendendo Flutter'), Task('Andar de Bike'), Task('Meditar'), Task('Estudar'), Task('Estudar'), Task('Estudar')],
+          children: [
+            Task('Aprendendo Flutter'),
+            Task('Andar de Bike'),
+            Task('Meditar'),
+            Task('Estudar'),
+            Task('Jogar videogame'),
+            Task('Desenhar')
+          ],
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {},
+          child: Icon(Icons.add),
+          backgroundColor: Colors.white,
         ),
       ),
     );
   }
 }
 
-class Task extends StatelessWidget {
-  final String nome;
-  const Task(this.nome, {super.key});
+class Task extends StatefulWidget {
+  final String name;
+  const Task(this.name, {super.key});
+
+  @override
+  State<Task> createState() => _TaskState();
+}
+
+class _TaskState extends State<Task> {
+  int nivel = 0;
 
   @override
   Widget build(BuildContext context) {
+
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Container(
@@ -47,24 +64,60 @@ class Task extends StatelessWidget {
               color: Colors.blue,
               height: 140,
             ),
-            Container(
-              color: Colors.white,
-              height: 100,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Container(
-                    color: Colors.black26,
-                    height: 100,
-                    width: 100,
+            Column(
+              children: [
+                Container(
+                  color: Colors.white,
+                  height: 100,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        color: Colors.black26,
+                        height: 100,
+                        width: 100,
+                      ),
+                      Container(
+                        width: 180,
+                        child: Text(
+                          widget.name,
+                          style: TextStyle(fontSize: 24),
+                          overflow: TextOverflow.ellipsis
+                        ),
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          setState(() {
+                            nivel++;
+                          });
+                          print(nivel);
+                        },
+                        child: Icon(Icons.arrow_drop_up),
+                        style: ElevatedButton.styleFrom(primary: Colors.blue, onPrimary: Colors.white),
+                      )
+                    ],
                   ),
-                  Text(nome),
-                  ElevatedButton(
-                    onPressed: () {},
-                    child: Icon(Icons.arrow_drop_up),
-                  )
-                ],
-              ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        child: LinearProgressIndicator(
+                          color: Colors.white,
+                          value: 1,
+                        ),
+                        width: 200,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text('Nível $nivel', style: TextStyle(color: Colors.white, fontSize: 16),),
+                    ),
+                  ],
+                ),
+              ],
             )
           ],
         ),
